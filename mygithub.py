a39450b62dac7ae439dd7e410263c658616e7dd1
Vlad Grin <vortexnz21@gmail.com>
2022-03-26 20:00:50 +0300

    Add parameter n instead of fixed 3 to github.py

--- mygithub.py
+++ mygithub.py
@@ -16,10 +16,10 @@ class MetOptRepo:
         src = req.text
         self._all_commits = json.loads(src)
 
-    def commits_to_str(self, name: str) -> str:
-        commits = self._choose_last_3_commits(name)
+    def commits_to_str(self, name: str, n: int) -> str:
+        commits = self._choose_last_n_commits(name, n)
         s = ''
-        for i in range(2, -1, -1):
+        for i in range(n-1, -1, -1):
             s += MetOptRepo._commit_to_str(commits[i]) + '\n'
         return s
 
@@ -30,11 +30,11 @@ class MetOptRepo:
             f"date:  {commit['commit']['author']['date'].replace('T', '  ')[:-4]}\n"
         return s
 
-    def _choose_last_3_commits(self, name):
+    def _choose_last_n_commits(self, name: str, n: int) -> list:
         try:
             login = MetOptRepo.team[name]
         except KeyError:
-            return self._all_commits[:3]
+            return self._all_commits[:n]
 
-        last_3_commits = [commit for commit in self._all_commits if commit['author']['login'] == login]
-        return last_3_commits
+        last_users_commits = [commit for commit in self._all_commits if commit['author']['login'] == login]
+        return last_users_commits
