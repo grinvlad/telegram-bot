cd5a15ab2fdaa7e35b12d0bd9111c5e10caed8fd
Vlad Grin <vortexnz21@gmail.com>
2022-04-10 16:46:00 +0300

    Add log decorator for functions in bot.py

--- bot.py
+++ bot.py
@@ -1,41 +1,37 @@
 import telebot
-import logging
 
 from credentials import config
 from mygithub import MetOptRepo
 from itmotable import ItmoTable
 from memes import Meme
 import itmotable
-
+from log import log
 
 bot = telebot.TeleBot(config.bot_token)
 
-logging.basicConfig(format='%(levelname)s | %(asctime)s | %(funcName)s | %(lineno)d | %(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p',
-                    filename='utils/bot.log',
-                    encoding='utf-8',
-                    level=logging.INFO)
 
 
 @bot.message_handler(commands=['start'])
+@log
 def send_welcome(message):
     pass
 
 
 @bot.message_handler(commands=['help'])
+@log
 def send_welcome(message):
     pass
 
 
 @bot.message_handler(commands=['commit'])
+@log
 def commit(message):
-    logging.info(f'{message.from_user.username} | {message.text}')
     process_printing_commits_step(message, n=1)
 
 
 @bot.message_handler(commands=['3commits'])
+@log
 def commits(message):
-    logging.info(f'{message.from_user.username} | {message.text}')
     markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
     markup.add(*MetOptRepo.team.keys(), 'всех')
     msg = bot.reply_to(message, 'Чьи коммиты посмотрим?', reply_markup=markup)
@@ -43,27 +39,25 @@ def commits(message):
 
 
 def process_printing_commits_step(message, n: int):
-    logging.info(f'{message.from_user.username} | {message.text}')
     repo = MetOptRepo()
     commits = repo.commits_to_str(message.text, n)
     bot.send_message(message.chat.id, commits)
 
 
 @bot.message_handler(commands=['itmotable'])
+@log
 def itmo_table(message):
-    logging.info(f'{message.from_user.username} | {message.text}')
     markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
     markup.add('Последнюю готовую', 'Новую')
-    file_name = ItmoTable.get_last_table_name(ItmoTable.get_last_table())
+    file_name = itmotable.get_last_table_name(itmotable.get_last_table())
     msg = bot.reply_to(message, f'Взять \n'
                                 f'{file_name} \n'
                                 f'или сделать новую?', reply_markup=markup)
     bot.register_next_step_handler(msg, process_send_itmo_table_step)
 
 
+@log
 def process_send_itmo_table_step(message):
-    log_message = f'{message.from_user.username} | {message.text}'
-    logging.info(log_message)
     if message.text == 'Последнюю готовую':
         pass
     elif message.text == 'Новую':
@@ -73,22 +67,20 @@ def process_send_itmo_table_step(message):
         except itmotable.HttpError:
             bot.send_message(message.chat.id, "Проблема с Google Sheets API.\n"
                                               "Отправлю пока последнюю созданную таблицу.")
-            logging.error(log_message)
         finally:
             bot.delete_message(tmp.chat.id, tmp.message_id)
     else:
         bot.reply_to(message, 'Тебе всего лишь надо было на кнопку нажать')
-        logging.warning(log_message)
         return
-    file = open(ItmoTable.get_last_table(), 'rb')
+    file = open(itmotable.get_last_table(), 'rb')
     bot.send_document(message.chat.id, file)
     file.close()
 
 
 @bot.message_handler(func=Meme.when_to_send_meme)
+@log
 def send_random_meme(message):
     bot.send_photo(message.chat.id, Meme.get_random_url())
-    logging.info(f'{message.from_user.username} | {message.text}')
 
 
 bot.infinity_polling()
