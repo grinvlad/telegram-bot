8bc2d2a26044f98cbc40dbe3134497b56d855924
Vlad Grin <vortexnz21@gmail.com>
2022-05-09 14:08:17 +0300

    Update bot.py Add new message_handlers for memes.py functionality

--- bot.py
+++ bot.py
@@ -1,17 +1,16 @@
-import random
-import re
-
 import telebot
+
+import memes
+import itmotable
 
 from credentials import config
 from mygithub import MetOptRepo
 from itmotable import ItmoTable
-from memes import Meme
-import itmotable
+from memes import MemeQue
 from log import log
 
-bot = telebot.TeleBot(config.bot_token)
 
+bot = telebot.TeleBot(config.bot_token)
 
 
 @bot.message_handler(commands=['start'])
@@ -43,8 +42,8 @@ def commits(message):
 
 def process_printing_commits_step(message, n: int):
     repo = MetOptRepo()
-    commits = repo.commits_to_str(message.text, n)
-    bot.send_message(message.chat.id, commits)
+    commits_ = repo.commits_to_str(message.text, n)
+    bot.send_message(message.chat.id, commits_)
 
 
 @bot.message_handler(commands=['itmotable'])
@@ -80,10 +79,36 @@ def process_send_itmo_table_step(message):
     file.close()
 
 
-@bot.message_handler(func=Meme.when_to_send_meme)
+@bot.message_handler(commands=['len_meme_queue'])
 @log
-def send_random_meme(message):
-    bot.send_photo(message.chat.id, Meme().get_random_url())
+def len_meme_queue(message):
+    que = MemeQue()
+    answer_message = f'Мемов в текущей очереди: {len(que)}'
+    bot.reply_to(message, answer_message)
+
+
+@bot.message_handler(chat_types=['private'], content_types=['text'], func=memes.when_to_add_new_meme)
+def add_meme(message):
+    url = message.text
+    index = MemeQue().add_new_meme(url)
+    answer_message = ('Добавила.\n'
+                      f'Позиция мема в текущей очереди: {index}')
+    bot.reply_to(message, answer_message)
+
+
+@bot.message_handler(chat_types=['private'], content_types=['text'], func=memes.when_to_illegally_add_new_meme)
+def illegally_add_meme(message):
+    option, url = message.text.split(maxsplit=1)
+    index = MemeQue().illegally_add_new_meme(url)
+    answer_message = (f'Добавила c опцией {option}.\n'
+                      f'Позиция мема в текущей очереди: {index}')
+    bot.reply_to(message, answer_message)
+
+
+@bot.message_handler(func=memes.when_to_send_meme)
+@log
+def send_next_meme(message):
+    bot.send_photo(message.chat.id, MemeQue().get_next_meme())
 
 
 bot.infinity_polling()
